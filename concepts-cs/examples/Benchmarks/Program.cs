e36e6e6d3b1ffe7754429642a7d325ba6a923976
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-24 20:27:31 +0500

    examples: added cases for benchmarks

--- concepts-cs/examples/Benchmarks/Program.cs
+++ concepts-cs/examples/Benchmarks/Program.cs
@@ -7,6 +7,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var summary = BenchmarkRunner.Run<CachedServerValuesBenchmark>();
+        var summary = BenchmarkRunner.Run<ValueEntryPassAndSortBenchmark>();
     }
 }
