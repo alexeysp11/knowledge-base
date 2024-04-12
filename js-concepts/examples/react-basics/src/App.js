674ed92dee76af4256657d37b5ee6c939896dba4
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-12 08:50:11 +0500

    js-concepts: added react-basics in examples

---
+++ js-concepts/examples/react-basics/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoList from "./Todo/TodoList";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>React test project</h1>
+
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
