157b81b6e2394ad769fb33e3bf285b263752254e
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-16 08:55:32 +0500

    examples: added template for AuthExamples

---
+++ concepts-cs/examples/AuthExamples/MvcAuthExample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcAuthExample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
