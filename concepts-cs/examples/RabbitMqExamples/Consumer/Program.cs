3c41efafbea1b1de98fb86a4f9a077ba646193ee
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-16 14:07:01 +0500

    examples: template for RabbitMqExamples

---
+++ concepts-cs/examples/RabbitMqExamples/Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Concepts.Examples.Publisher;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Configurations.
+        var queueName = "dev-queue";
+
+        // Establish connection.
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += Consumer_Received;
+            
+            channel.BasicConsume(queue: queueName, 
+                autoAck: true, 
+                consumer: consumer);
+            Console.WriteLine($"Subscribed to the '{queueName}'");
+            Console.ReadLine();
+        }
+    }
+
+    private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+    {
+        var body = e.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+        Console.WriteLine($"- Received message from the RMQ: '{message}'");
+    }
+}
---
+++ concepts-cs/examples/RabbitMqExamples/Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Concepts.Examples.Publisher;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Configurations.
+        var queueName = "dev-queue";
+        var exchangeName = "dev-ex";
+        var message = $"Message for the RMQ queue '{queueName} from Publisher'";
+
+        // Establish connection.
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            channel.BasicPublish(exchange: "", 
+                routingKey: queueName,
+                basicProperties: null,
+                body: Encoding.UTF8.GetBytes(message));
+            Console.WriteLine($"Message is successfully sent to the RMQ server");
+        }
+    }
+}
