f74ecc8233b18c9b198c4483281451fc78001ce7
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-17 13:47:02 +0500

    examples: added templates for MultithreadingTests

--- concepts-cs/examples/MultithreadingTests/Program.cs
+++ concepts-cs/examples/MultithreadingTests/Program.cs
@@ -4,7 +4,8 @@ public class Program
 {
     public static void Main()
     {
-        var instance = new SynchronizationPromitives();
+        // var instance = new SynchronizationPromitives();
+        var instance = new TestConfigureAwait();
         instance.Execute();
     }
 }
