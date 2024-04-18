d8f36ed69681dc503c6d033ab0a831eb5fcaa185
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-18 14:06:10 +0500

    examples: direct exchange in RabbitMqExamples

--- concepts-cs/examples/RabbitMqExamples/Consumer/Program.cs
+++ concepts-cs/examples/RabbitMqExamples/Consumer/Program.cs
@@ -2,11 +2,50 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 
-namespace Concepts.Examples.Publisher;
+namespace Concepts.Examples.Consumer;
 
 public class Program
 {
     public static void Main()
+    {
+        //RunDefaultExchangeConsumer();
+        RunDirectExchangeConsumer();
+    }
+
+    private static void RunDirectExchangeConsumer()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queue: queueName,
+                                        exchange: "direct_logs",
+                                        routingKey: "error");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body;
+                var message = Encoding.UTF8.GetString(body.ToArray());
+                Console.WriteLine(" Received message: '{0}' from the queue '{1}'", message, queueName);
+            };
+
+            channel.BasicConsume(queue: queueName,
+                                    autoAck: true,
+                                    consumer: consumer);
+
+            Console.WriteLine($"Subscribed to the queue '{queueName}'");
+
+            Console.ReadLine();
+        }
+    }
+
+    private static void RunDefaultExchangeConsumer()
     {
         // Configurations.
         var queueName = "dev-queue";
@@ -24,9 +63,9 @@ public class Program
             var consumer = new EventingBasicConsumer(channel);
 
             consumer.Received += Consumer_Received;
-            
-            channel.BasicConsume(queue: queueName, 
-                autoAck: true, 
+
+            channel.BasicConsume(queue: queueName,
+                autoAck: true,
                 consumer: consumer);
             Console.WriteLine($"Subscribed to the '{queueName}'");
             Console.ReadLine();
--- concepts-cs/examples/RabbitMqExamples/Publisher/Program.cs
+++ concepts-cs/examples/RabbitMqExamples/Publisher/Program.cs
@@ -7,9 +7,54 @@ public class Program
 {
     public static void Main()
     {
+        //RunDefaultExchange();
+        RunDirectExchange();
+    }
+
+    private static void RunDirectExchange()
+    {
+        Task.Run(CreateTaskDirectExchange(12000, "error"));
+        Task.Run(CreateTaskDirectExchange(10000, "info"));
+        Task.Run(CreateTaskDirectExchange(8000, "warning"));
+
+        Console.ReadKey();
+    }
+
+    static Func<Task> CreateTaskDirectExchange(int timeToSleepTo, string routingKey)
+    {
+        return () =>
+        {
+            var counter = 0;
+            do
+            {
+                int timeToSleep = new Random().Next(1000, timeToSleepTo);
+                Thread.Sleep(timeToSleep);
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+                    string message = $"Message type [{routingKey}] from publisher N {counter}";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "direct_logs",
+                                        routingKey: routingKey,
+                                        basicProperties: null,
+                                        body: body);
+
+                    Console.WriteLine($"Message type [{routingKey}] is sent into Direct Exchange [N:{counter++}]");
+                }
+            } while (true);
+        };
+    }
+
+    private static void RunDefaultExchange()
+    {
         // Configurations.
         var queueName = "dev-queue";
-        var exchangeName = "dev-ex";
         var message = $"Message for the RMQ queue '{queueName} from Publisher'";
 
         // Establish connection.
@@ -22,7 +67,7 @@ public class Program
                 exclusive: false,
                 autoDelete: false,
                 arguments: null);
-            channel.BasicPublish(exchange: "", 
+            channel.BasicPublish(exchange: "",
                 routingKey: queueName,
                 basicProperties: null,
                 body: Encoding.UTF8.GetBytes(message));
