c011d6bab00f06a616bba0cc13df6d70ac7a773e
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-19 09:06:18 +0500

    js-concepts: edited react example

--- js-concepts/examples/react-basics/src/App.js
+++ js-concepts/examples/react-basics/src/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import TodoList from "./Todo/TodoList";
 
 function App() {
+  const todos = [
+    {id: 1, completed: false, title: 'Buy milk'},
+    {id: 2, completed: false, title: 'Buy bread'},
+    {id: 3, completed: false, title: 'Buy pizza'}
+  ];
+
   return (
     <div className="wrapper">
       <h1>React test project</h1>
 
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   );
 }
--- js-concepts/examples/react-basics/src/Todo/TodoList.js
+++ js-concepts/examples/react-basics/src/Todo/TodoList.js
@@ -1,11 +1,20 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
-export default function TodoList() {
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
+
+export default function TodoList(props) {
     return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+        <ul style={styles.ul}>
+            {props.todos.map((todo, index) => {
+                return <TodoItem todo={todo} key={todo.id} index={index} />
+            })}
         </ul>
     )
 }
