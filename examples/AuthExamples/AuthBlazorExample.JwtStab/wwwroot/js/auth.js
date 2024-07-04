d581f18f5659c813cfd849b2b61e77991a48bd9a
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-07-04 11:55:17 +0500

    issue-21: template for sending auth http request and saving cookies

---
+++ examples/AuthExamples/AuthBlazorExample.JwtStab/wwwroot/js/auth.js
@@ -0,0 +1,29 @@
+function validateUser(login, password) {
+    // Create request object.
+    const request = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ login: login, password: password })
+    };
+    document.cookie = 'authblazorexample-cookies=' + login;
+
+    // Send HTTP request.
+    // fetch('/api/Auth/ValidateUser', request)
+    //     .then(response => {
+    //         if (response.ok) {
+    //             // Get cookies from response.
+    //             // response.headers.forEach((value, name) => {
+    //             //     // Add cookies into browser.
+    //             //     document.cookie = ${name}=${value}; path=/;
+    //             // });
+    //             // Redirect to root.
+    //         } else {
+    //             console.error('Authentication error!');
+    //         }
+    //     })
+    //     .catch(error => {
+    //         console.error('Error sending request:', error);
+    //     });
+}
