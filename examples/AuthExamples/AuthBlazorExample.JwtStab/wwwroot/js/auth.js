1a772b0f5f20908d7c92eb52c16b61f72edc574a
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-07-11 10:53:21 +0500

    issue-21: added auth controller

--- examples/AuthExamples/AuthBlazorExample.JwtStab/wwwroot/js/auth.js
+++ examples/AuthExamples/AuthBlazorExample.JwtStab/wwwroot/js/auth.js
@@ -7,23 +7,25 @@ function validateUser(login, password) {
         },
         body: JSON.stringify({ login: login, password: password })
     };
-    document.cookie = 'authblazorexample-cookies=' + login;
+    //document.cookie = 'authblazorexample-cookies=' + login;
+    console.log(request.body);
 
     // Send HTTP request.
-    // fetch('/api/Auth/ValidateUser', request)
-    //     .then(response => {
-    //         if (response.ok) {
-    //             // Get cookies from response.
-    //             // response.headers.forEach((value, name) => {
-    //             //     // Add cookies into browser.
-    //             //     document.cookie = ${name}=${value}; path=/;
-    //             // });
-    //             // Redirect to root.
-    //         } else {
-    //             console.error('Authentication error!');
-    //         }
-    //     })
-    //     .catch(error => {
-    //         console.error('Error sending request:', error);
-    //     });
+    fetch('/api/Auth/ValidateUser', request)
+        .then(response => {
+            if (response.ok) {
+                // Get cookies from response.
+                response.headers.forEach((value, name) => {
+                    // Add cookies into browser.
+                    console.log(name + '=' + value);
+                    document.cookie = name + '=' + value; path='/';
+                });
+                // Redirect to root.
+            } else {
+                console.error('Authentication error!');
+            }
+        })
+        .catch(error => {
+            console.error('Error sending request:', error);
+        });
 }
