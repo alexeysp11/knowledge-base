fbabfff166333b8a38a1c255d55b565cc26f5911
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-09-03 11:07:31 +0500

    issue-60: fixed typos

---
+++ src/examples/WorkerServiceXplatform/WorkerServiceXplatform/AppInitConfigs.cs
@@ -0,0 +1,12 @@
+namespace KnowledgeBase.Examples.WorkerServiceXplatform;
+
+/// <summary>
+/// Contains application configuration settings during startup.
+/// </summary>
+public class AppInitConfigs
+{
+    /// <summary>
+    /// Connection string for database.
+    /// </summary>
+    public string? DbConnectionString { get; set; }
+}
