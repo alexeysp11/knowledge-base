b9715325dc1b79d2bcbce0cf77eae0a0035f096d
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-09-03 11:34:51 +0500

    issue-60: fixed typos

--- src/examples/WokerServiceXplatform/WokerServiceXplatform/sccreate.bat
+++
@@ -1,5 +0,0 @@
-
-sc stop KnoledgeBase.Examples.WorkerServiceLogs
-sc delete KnoledgeBase.Examples.WorkerServiceLogs
-
-sc create KnoledgeBase.Examples.WorkerServiceLogs binPath="C:\ExampleServices\knowledge-base\WokerServiceXplatform\WokerServiceXplatform.exe" DisplayName= "KnoledgeBase.Examples.WorkerServiceLogs"
