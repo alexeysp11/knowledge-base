7f00b51cd13600a7ae9425293206ed0fb3025595
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-10-26 15:52:16 +0500

    issue-68: generate pdf from code

--- src/examples/AutoDocsExamples/DocfxExamples/Program.cs
+++ src/examples/AutoDocsExamples/DocfxExamples/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("docfx example");
+
+var autoDocsStaticFiles = "C:\\Users\\User\\Documents\\auto-docs\\knowledge-base\\AutoDocsExamples\\docfx.json";
+
+Console.WriteLine("docfx example: yml");
+await Docfx.Dotnet.DotnetApiCatalog.GenerateManagedReferenceYamlFiles(autoDocsStaticFiles);
+
+Console.WriteLine("docfx example: build");
+await Docfx.Docset.Build(autoDocsStaticFiles);
+
+Console.WriteLine("docfx example: pdf");
+await Docfx.Docset.Pdf(autoDocsStaticFiles);
