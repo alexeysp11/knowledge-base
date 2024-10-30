bfb2e2357f6694358a463da5860c81a0a442e1e3
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-10-31 00:22:45 +0500

    issue-82: use signature of the last committer for git merge

--- src/examples/GitToolsExamples/LibGit2SharpExample/Program.cs
+++ src/examples/GitToolsExamples/LibGit2SharpExample/Program.cs
@@ -4,23 +4,30 @@ using LibGit2Sharp.Handlers;
 
 Console.WriteLine("LibGit2Sharp example");
 
-Identity identity = new Identity("alexeysp11", "alexeyspiridon1111@gmail.com");
-Signature signature = new Signature(identity, new DateTimeOffset(2024, 10, 30, 19, 54, 00, TimeSpan.FromHours(2)));
-
 string pathToRepo = @"C:\Users\User\Documents\proj\delivery-service-csharp";
 string logMessage = "";
 using (var repo = new Repository(pathToRepo))
 {
+    // Start interacting with git.
     System.Console.WriteLine($"Imitate git pull");
 
+    // Get the origin remote.
     var remote = repo.Network.Remotes["origin"];
     var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
 
-    System.Console.WriteLine($"git fetch");
+    // Fetch changes.
+    System.Console.WriteLine($"1. git fetch");
     Commands.Fetch(repo, remote.Name, refSpecs, null, logMessage);
 
-    System.Console.WriteLine($"git merge");
-    MergeResult mergeResult = repo.Merge(repo.Branches["main"].Tip, signature);
+    // Get the last commit, its author and message.
+    System.Console.WriteLine($"2. Getting the last commit");
+    Commit branchTip = repo.Branches["main"].Tip;
+    Signature branchTipAuthor = branchTip.Author;
+    System.Console.WriteLine($"{branchTipAuthor.When} | {branchTip.MessageShort}");
+
+    // Merge changes locally.
+    System.Console.WriteLine($"3. git merge");
+    MergeResult mergeResult = repo.Merge(branchTip, branchTipAuthor);
     System.Console.WriteLine($"merge status: {mergeResult.Status}");
 }
 Console.WriteLine(logMessage);
