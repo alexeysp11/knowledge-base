2d99bf1b80599c720997e90af1f93de74911c297
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-07-31 00:10:54 +0500

    issue-94: filter

---
+++ src/dsa/KnowledgeBase.DataStructuresAlgorithms/CollectionProc/ObjectFilterByPredicats.cs
@@ -0,0 +1,27 @@
+﻿namespace KnowledgeBase.DataStructuresAlgorithms.CollectionProc
+{
+    public static class ObjectFilterByPredicats
+    {
+        public static List<string> Filter(List<string> inputList, Func<string, bool> func)
+        {
+            if (inputList == null || !inputList.Any())
+            {
+                return new List<string>();
+            }
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func), "Predicate could not be null");
+            }
+
+            var result = new List<string>();
+            foreach (string item in inputList)
+            {
+                if (func(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+    }
+}
