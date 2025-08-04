a816746326e6af31555221607a3e6f01a6ea460e
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-08-04 11:19:38 +0500

    issue-95: ResultContainer

---
+++ src/examples/MqInsideHttp/MqInsideHttp.WebApi/Models/MqInsideHttpResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MqInsideHttp.WebApi.Models
+{
+    public class MqInsideHttpResponse
+    {
+        public string? RequestId { get; set; }
+        public string? Body { get; set; }
+        public string? StatusCode { get; set; }
+    }
+}
---
+++ src/examples/MqInsideHttp/MqInsideHttp.WebApi/Models/ResultContainer.cs
@@ -0,0 +1,56 @@
+﻿namespace MqInsideHttp.WebApi.Models
+{
+    internal static class ResultContainer
+    {
+        private static List<string> _requestIdList;
+        private static Dictionary<string, MqInsideHttpResponse> _responses;
+
+        static ResultContainer()
+        {
+            _requestIdList = new List<string>();
+            _responses = new Dictionary<string, MqInsideHttpResponse>();
+        }
+
+        internal static void AddRequestId(string requestId)
+        {
+            lock (_requestIdList)
+            {
+                if (!_requestIdList.Contains(requestId))
+                {
+                    _requestIdList.Add(requestId);
+                }
+            }
+        }
+
+        internal static MqInsideHttpResponse? GetResponseByRequestId(string requestId)
+        {
+            lock (_responses)
+            {
+                if (_responses.ContainsKey(requestId))
+                {
+                    return _responses[requestId];
+                }
+            }
+            return null;
+        }
+
+        internal static List<string> GetRequestIdList()
+        {
+            lock (_requestIdList)
+            {
+                return new List<string>(_requestIdList);
+            }
+        }
+
+        internal static void AddResponse(string requestId, MqInsideHttpResponse response)
+        {
+            lock (_responses)
+            {
+                if (!_responses.ContainsKey(requestId))
+                {
+                    _responses.Add(requestId, response);
+                }
+            }
+        }
+    }
+}
